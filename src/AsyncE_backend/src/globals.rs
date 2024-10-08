f48833847140c2a8a9025fbfb9935124ccdf88e7
zKevz <andriankevin69@gmail.com>
2024-10-08 16:11:53 +0700

    fixes

--- src/AsyncE_backend/src/globals.rs
+++ src/AsyncE_backend/src/globals.rs
@@ -1,10 +1,10 @@
-use std::{cell::RefCell, collections::HashMap};
+use std::{cell::RefCell, collections::BTreeMap};
 
 use candid::Principal;
 
 use crate::{group::Group, user::User};
 
 thread_local! {
-    pub static USERS: RefCell<HashMap<Principal, User>> = RefCell::default();
-    pub static GROUPS: RefCell<HashMap<String, Group>> = RefCell::default();
+    pub static USERS: RefCell<BTreeMap<Principal, User>> = RefCell::default();
+    pub static GROUPS: RefCell<BTreeMap<String, Group>> = RefCell::default();
 }
