b05b5c3edb8697c5cfa033e8a6a9bd62eba52017
zKevz <andriankevin69@gmail.com>
2024-10-08 22:23:26 +0700

    save & load

--- src/AsyncE_backend/src/globals.rs
+++ src/AsyncE_backend/src/globals.rs
@@ -4,7 +4,10 @@ use candid::Principal;
 
 use crate::{group::Group, user::User};
 
+pub type UserStore = BTreeMap<Principal, User>;
+pub type GroupStore = BTreeMap<String, Group>;
+
 thread_local! {
-    pub static USERS: RefCell<BTreeMap<Principal, User>> = RefCell::default();
-    pub static GROUPS: RefCell<BTreeMap<String, Group>> = RefCell::default();
+    pub static USERS: RefCell<UserStore> = RefCell::default();
+    pub static GROUPS: RefCell<GroupStore> = RefCell::default();
 }
--- src/AsyncE_backend/src/lib.rs
+++ src/AsyncE_backend/src/lib.rs
@@ -4,11 +4,29 @@ pub mod globals;
 pub mod group;
 pub mod user;
 
+use crate::{group::Group, user::User};
 use getrandom::register_custom_getrandom;
+use globals::{GROUPS, USERS};
 
 fn custom_getrandom(_buf: &mut [u8]) -> Result<(), getrandom::Error> {
-    // TODO get some randomness
     return Ok(());
 }
 
 register_custom_getrandom!(custom_getrandom);
+
+#[ic_cdk::pre_upgrade]
+fn pre_upgrade() {
+    let users_store = USERS.with_borrow(|users| users.clone());
+    let groups_store = GROUPS.with_borrow(|groups| groups.clone());
+
+    ic_cdk::storage::stable_save((users_store, groups_store)).unwrap();
+}
+
+#[ic_cdk::post_upgrade]
+fn post_upgrade() {
+    let (user_store, group_store) = ic_cdk::storage::stable_restore().unwrap();
+    USERS.with_borrow_mut(|users| *users = user_store);
+    GROUPS.with_borrow_mut(|groups| *groups = group_store);
+}
+
+ic_cdk::export_candid!();
