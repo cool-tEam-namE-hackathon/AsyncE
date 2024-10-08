9b1ee4c0e8127e87193019bb7933645ddbd12daf
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2024-10-08 13:10:15 +0700

    setup project frontend

--- src/AsyncE_frontend/src/main.js
+++ src/AsyncE_frontend/src/main.js
@@ -1,6 +1,6 @@
-import { createPinia } from 'pinia';
-import { createApp } from 'vue';
-import './index.scss';
-import App from './App.vue';
+import { createPinia } from "pinia";
+import { createApp } from "vue";
+import "@/index.css";
+import App from "@/App.vue";
 
-createApp(App).use(createPinia()).mount('#app');
+createApp(App).use(createPinia()).mount("#app");
