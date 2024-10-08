094efe4768fa62c8f9e7be2e91c6621ad3c82ccf
zKevz <andriankevin69@gmail.com>
2024-10-08 16:59:46 +0700

    fix compile

--- src/AsyncE_backend/src/lib.rs
+++ src/AsyncE_backend/src/lib.rs
@@ -3,3 +3,12 @@
 pub mod globals;
 pub mod group;
 pub mod user;
+
+use getrandom::register_custom_getrandom;
+
+fn custom_getrandom(_buf: &mut [u8]) -> Result<(), getrandom::Error> {
+    // TODO get some randomness
+    return Ok(());
+}
+
+register_custom_getrandom!(custom_getrandom);
