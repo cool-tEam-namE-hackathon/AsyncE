4b613448dddef8c2c9a20aefdb278057b6c10a34
dylan0804 <dylan.halim@binus.ac.id>
2024-10-09 08:42:16 +0700

    fix: bug

--- src/AsyncE_frontend/src/App.vue
+++ src/AsyncE_frontend/src/App.vue
@@ -9,10 +9,7 @@
 <script setup lang="ts">
 import Navbar from "@components/layout/Navbar.vue";
 import Footer from "@components/layout/Footer.vue";
-// import { ref } from "vue";
-
 // let greeting = ref("");
-let be = AsyncE_backend;
 
 // async function handleSubmit(e) {
 //     e.preventDefault();
