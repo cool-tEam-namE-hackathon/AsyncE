aa6f44eae13cdbc9e6033d016199a88cebafede9
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2024-11-07 15:10:54 +0700

    improve home page content

--- src/AsyncE_frontend/src/data/home_page.ts
+++ src/AsyncE_frontend/src/data/home_page.ts
@@ -1,4 +1,4 @@
-export default [
+const home_images = [
     {
         left: { image_url: "/images/home_2.webp", text: "hey" },
         right: { image_url: "/images/home_4.webp", text: "hey" },
@@ -12,3 +12,11 @@ export default [
         right: { image_url: "/images/home_5.webp", text: "hey" },
     },
 ];
+
+const meet_texts = [
+    { text: "your own schedule", color: "text-green-500" },
+    { text: "your own place", color: "text-red-500" },
+    { text: "your own time", color: "text-blue-500" },
+];
+
+export { home_images, meet_texts };
